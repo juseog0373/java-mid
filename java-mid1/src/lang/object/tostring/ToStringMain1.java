b45eed9c18e664e5b9c1660f596bc06d2e03885c
jskim <juseog0373@naver.com>
2024-08-02 17:59:32 +0900

    toString()

---
+++ java-mid1/src/lang/object/tostring/ToStringMain1.java
@@ -0,0 +1,15 @@
+package lang.object.tostring;
+
+public class ToStringMain1 {
+    public static void main(String[] args) {
+        Object obj = new Object();
+        String string = obj.toString();
+
+        // toString() 출력
+        System.out.println(string);
+
+        // object 직접 출력
+        System.out.println(obj);
+
+    }
+}
